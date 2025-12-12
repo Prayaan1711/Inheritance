99c5241380f129ce6697e6b4656f2bfbc9e19d98
Prayaan1711 <spyfyi1711@gmail.com>
2025-12-12 13:28:46 +0530

    1234

---
+++ practice.py
@@ -0,0 +1,74 @@
+print("Lets find out your favourite player!")
+
+team = input("Enter your favourite team from Bayern, Madrid, Barca, PSG, ManCity, Liverpool, Milan: ")
+if team == "Bayern":
+    position = input("Enter your position from ATK, MID, DEF, GK: ")
+    if position == "ATK":
+        print("Your fav player is Kane")
+    elif position == "MID":
+        print("Your fav player is Kimmich")
+    elif position == "DEF":
+        print("Your fav player is Davies")
+    elif position == "GK":
+        print("Your fav player is Neuer")
+if team == "Madrid":
+    position = input("Enter your position from ATK, MID, DEF, GK: ")
+    if position == "ATK":
+        print("Your fav player is Mbappe")
+    elif position == "MID":
+        print("Your fav player is Bellingham")
+    elif position == "DEF":
+        print("Your fav player is Militao")
+    elif position == "GK":
+        print("Your fav player is Courtois")
+if team == "Barca":
+    position = input("Enter your position from ATK, MID, DEF, GK: ")
+    if position == "ATK":
+        print("Your fav player is Lamine Yamal")
+    elif position == "MID":
+        print("Your fav player is Dani Olmo")
+    elif position == "DEF":
+        print("Your fav player is Aroujo")
+    elif position == "GK":
+        print("Your fav player is Ter Stegen")
+if team == "PSG":
+    position = input("Enter your position from ATK, MID, DEF, GK: ")
+    if position == "ATK":
+        print("Your fav player is Dembele")
+    elif position == "MID":
+        print("Your fav player is Vitinha")
+    elif position == "DEF":
+        print("Your fav player is Nunu.Mendes")
+    elif position == "GK":
+        print("Your fav player is Donnurumma")
+if team == "ManCity":
+    position = input("Enter your position from ATK, MID, DEF, GK: ")
+    if position == "ATK":
+        print("Your fav player is Haaland")
+    elif position == "MID":
+        print("Your fav player is Rodri")
+    elif position == "DEF":
+        print("Your fav player is Ake")
+    elif position == "GK":
+        print("Your fav player is Ederson")
+if team == "Liverpool":
+    position = input("Enter your position from ATK, MID, DEF, GK: ")
+    if position == "ATK":
+        print("Your fav player is Salah")
+    elif position == "MID":
+        print("Your fav player is Macalister")
+    elif position == "DEF":
+        print("Your fav player is Van Dijk")
+    elif position == "GK":
+        print("Your fav player is Allison")
+if team == "Milan":
+    position = input("Enter your position from ATK, MID, DEF, GK: ")
+    if position == "ATK":
+        print("Your fav player is R.Leoa")
+    elif position == "MID":
+        print("Your fav player is Modric")
+    elif position == "DEF":
+        print("Your fav player is Hernandez")
+    elif position == "GK":
+        print("Your fav player is Maignon")
+
